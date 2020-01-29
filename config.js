48bb99418ce1f90eaf62d1ddd58cff4ee113cf3b
Morris Miller <morris.miller@gmail.com>
2020-01-29 13:30:28 -0500

    reskinned

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    pageTitle: 'millennial marketing group',
-    promoText: 'free shipping!',
-    comparePrice: 999.99,
-    sellingPrice: 699.99,
-    productName: "Jeezy's",
-    productDescription: 'These kicks are the freshest',
+    pageTitle: 'The Original Galaxy Rose',
+    promoText: 'Free Shipping!',
+    comparePrice: 24.99,
+    sellingPrice: 18.99,
+    productName: "Original Galaxy Rose",
+    productDescription: "This beautifully crafted glass rose sculpture features a captivating display of colorful lights you will want to gaze into again and again. A unique gift for a special person. Order now and pay no shipping costs!",
     contactEmail: 'millennialmarketinggroup@gmail.com'
 };
